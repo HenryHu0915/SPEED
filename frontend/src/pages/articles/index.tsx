ffb08215a0b3ed2cc5dec253c1b55decbe9c84ed
HenryHu0915 <mvf6715@autuni.ac.nz>
2023-10-09 15:31:31 +1300

    frontend search, moderator, rate implement

---
+++ frontend/src/pages/articles/index.tsx
@@ -0,0 +1,74 @@
+import { GetStaticProps, NextPage } from "next";
+import SortableTable from "../../components/table/SortableTable";
+import axios from "axios";
+import styles from "./UserView.module.scss";
+
+
+interface ArticlesInterface {
+    id: string;
+    title: string;
+    authors: string;
+    source: string;
+    publication_year: string;
+    doi: string;
+    SE_practice: string;
+    claim: string;
+    averageRating: string;
+    approved: boolean;
+}
+
+type ArticlesProps = {
+  articles: ArticlesInterface[];
+};
+
+const Articles: NextPage<ArticlesProps> = ({ articles }) => {
+    const headers: { key: keyof ArticlesInterface; label: string }[] = [
+        { key: "title", label: "Title" },
+        { key: "authors", label: "Authors" },
+        { key: "source", label: "Source" },
+        { key: "publication_year", label: "Publication Year" },
+        { key: "doi", label: "DOI" },
+        { key: "SE_practice", label: "SE Practice" },
+        { key: "claim", label: "Claim" },
+        { key: "averageRating", label: "Rating" },
+    ];
+
+    return (
+        <div className={styles.container}>
+            <h1>SPEED Articles</h1>
+            <p>Search Placeholder</p>
+            <SortableTable headers={headers} data={articles} />
+        </div>
+    );
+};
+
+export const getStaticProps: GetStaticProps<ArticlesProps> = async (_) => {
+  try {
+    // Fetch articles from the API endpoint
+    const response = await axios.get("https://speed-backend-git-testing-leo-r-jia.vercel.app/api/articles");
+
+    // Extract the articles from the API response data
+    const articles: ArticlesInterface[] = response.data;
+
+    // Filter the articles to only include approved ones
+    const approvedArticles = articles.filter((article) => article.approved === true);
+
+    return {
+      props: {
+        articles: approvedArticles,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data from the API:", error);
+    return {
+      props: {
+        articles: [],
+      },
+    };
+  }
+};
+
+
+
+
+export default Articles; 
