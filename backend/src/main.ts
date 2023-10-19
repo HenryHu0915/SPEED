c7cfc67b2fc472ca346e0dc50c3105319ed6aab0
OneMassivepersonality <50425915+NotReallyBenjamin@users.noreply.github.com>
2023-10-20 11:40:14 +1300

    Chnaged Cors in db.js

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express/adapters/express-adapter';
 
+const localURL = 'http://localhost:3000';
+const vercelURL = 'https://speed-1-frontend-depo55qng-notreallybenjamins-projects.vercel.app';
 
   const CORS_OPTIONS = {
-    origin: 'http://localhost:3000', // Replace with the actual origin of your frontend
+    origin: localURL, // Replace with the actual origin of your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue : false,
     credentials: true,
   };
 
   async function bootstrap() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter());
+
     app.enableCors(CORS_OPTIONS);
 
     await app.listen(3001); // You can specify the port you want to listen on
